a21ebeaac1cf30d0d3cc15eca9d956f75c88e548
YeferVG <92173102+YeanVega@users.noreply.github.com>
2021-12-06 22:23:54 -0500

    Cambio de enseñanza a ensenanza

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -2,12 +2,12 @@ package comunicacion;
 
 public class Fabula extends Escrito {
 	
-	private String enseñanza;
+	private String ensenanza;
 	private String interpretacion;
 	
-	public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion) {
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
 		super(origen, titulo, autor, paginas);
-		this.enseñanza = enseñanza;
+		this.ensenanza = ensenanza;
 		this.interpretacion = interpretacion;
 	}
 	
@@ -16,11 +16,11 @@ public class Fabula extends Escrito {
 	}
 
 	public String getEnseñanza() {
-		return enseñanza;
+		return ensenanza;
 	}
 
-	public void setEnseñanza(String enseñanza) {
-		this.enseñanza = enseñanza;
+	public void setEnseñanza(String ensenanza) {
+		this.ensenanza = ensenanza;
 	}
 
 	public String getInterpretacion() {
@@ -39,7 +39,7 @@ public class Fabula extends Escrito {
 	@Override
 	public String toString() {
 		return getOrigen() + "\n" + getTitulo() + "\n" + getAutor() + "\n" 
-				+ getPaginas() + "\n" + enseñanza + "\n" + interpretacion;
+				+ getPaginas() + "\n" + ensenanza + "\n" + interpretacion;
 	}
 	
 	
