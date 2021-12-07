beaa146bf290a5c4cc459859bf14236823be00db
YeferVG <92173102+YeanVega@users.noreply.github.com>
2021-12-06 22:20:58 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -14,7 +14,33 @@ public class Fabula extends Escrito {
 	public String interpretacion() {
 		return interpretacion;
 	}
+
+	public String getEnseñanza() {
+		return enseñanza;
+	}
+
+	public void setEnseñanza(String enseñanza) {
+		this.enseñanza = enseñanza;
+	}
+
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
 	
+	@Override
+	int palabrasTotales(int palabrasPagina) {
+		return getPaginas()*palabrasPagina;
+	}
+	
+	@Override
+	public String toString() {
+		return getOrigen() + "\n" + getTitulo() + "\n" + getAutor() + "\n" 
+				+ getPaginas() + "\n" + enseñanza + "\n" + interpretacion;
+	}
 	
 	
 	
